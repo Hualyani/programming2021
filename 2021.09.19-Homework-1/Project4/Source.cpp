4381320f8251859104a3c0512952861a7967fffb
Шевченко Мария Александровна <st095928@student.spbu.ru>
2021-09-23 11:05:34 +0300

    HW1 task 5

---
+++ 2021.09.19-Homework-1/Project4/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int    main()
+{
+	int v = 0;
+	int t = 0;
+	int s = 0;
+	int a = 0;
+	int b = 0;
+	cin >> v;
+	cin >> t;
+	a = v * t;
+		
+	b = a / S;
+		
+	S = S * b;
+		
+	a = a - S;
+		
+	cout << "Вася остановится через " << t << " часов на отметке  " << a << " км" << endl;
+
+	
+ return EXIT_SUCCESS;
+}
